2d913516962b4972fd745d0f72d5105a473d6c50
N.S <naitoshunsuke7110@gmail.com>
2025-01-29 16:21:08 +0900

    関数をたくさん追加したで

--- img.h
+++ img.h
@@ -7,6 +7,10 @@ void img_putpixel(struct color c, int x, int y);
 void img_fillcircle(struct color c, double x, double y, double r);
 void gradation_square(struct color c, struct color d, double x,double y, double r, int steps);  //(下端の色, 上端の色, 中心x, 中心y, 一辺の長さ, 色の遷移回数=フレーム数)
 void img_circle(struct color c, double x, double y, double r);
-void write_U(void);
-void write_E(void);
-void write_C(void);
+void img_square(struct color c, double x, double y, double s, double t);
+void write_Uoutside(void);
+void write_Uinside(void);
+void write_Eoutside(void);
+void write_Einside(void);
+void write_Coutside(void);
+void write_Cinside(void);
